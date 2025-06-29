c2c049ec259fcf701fa5e03bc228e190ae21f615
Praneesha <pranesha182004@gmail.com>
2025-06-29 19:30:01 +0530

    changed user subjects params

--- app/api/subjects/getOne/[id]/route.ts
+++ app/api/subjects/getOne/[id]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest } from "next/server";
 // /app/api/subjects/getOne/[id]/route.ts
 export async function GET(
   req: NextRequest,
-  context: { params: { id: string } }
+  {params}: { params: { id: string } }
 ) {
-  return getSubjectByIdController(req, context.params.id);
+  return getSubjectByIdController(req, params.id);
 }
